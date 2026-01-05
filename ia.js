488320793615f6030705f49b57711d1eba4aff71
soportersbco-ops <soportersbco@gmail.com>
2026-01-05 13:08:14 -0500

    Remove existing handler function in ia.js

--- ia.js
+++ ia.js
@@ -1,12 +1,1 @@
-exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      mensaje: "La función de IA funciona correctamente 🚀"
-    })
-  };
-};
+ASDASDASD
