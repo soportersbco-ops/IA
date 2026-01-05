3d63a6fda0451b470fed68917d61cb09088cd0a0
soportersbco-ops <soportersbco@gmail.com>
2026-01-05 13:00:31 -0500

    Add serverless function for IA response

---
+++ netlify/functions/ia.js
@@ -0,0 +1,12 @@
+exports.handler = async () => {
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      mensaje: "IA funcionando correctamente 🚀"
+    })
+  };
+};
